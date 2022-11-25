d7bdc07c0f91c7a9cf6b156359e96e03d3796de1
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-11-25 11:32:42 +0300

    Add post styles

--- src/features/posts/Posts.js
+++ src/features/posts/Posts.js
@@ -1,7 +1,41 @@
-import React from 'react';
+// @ts-nocheck
+import React, { useEffect, useState } from 'react';
 
 const Posts = () => {
-    return <div>Posts</div>;
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3004/posts')
+            .then((res) => res.json())
+            .then((data) => setPosts(data));
+    }, []);
+
+    return posts.map((post) => (
+        <div key={post.id} style={styles.container}>
+            <img src={post.image} alt={post.title} style={styles.image} />
+            <div style={styles.textsContainer}>
+                <h2>{post.title}</h2>
+                <p>{post.author}</p>
+            </div>
+        </div>
+    ));
+};
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        padding: '10px',
+    },
+    textsContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '10px',
+        gap: '10px',
+    },
+    image: {
+        width: '100px',
+        height: '150px',
+    },
 };
 
 export default Posts;
