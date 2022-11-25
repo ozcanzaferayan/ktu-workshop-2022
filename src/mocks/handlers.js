1b86388d5b31f1f7f07726c0b94913fa92ada491
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-11-25 13:48:52 +0300

    Add mock server

---
+++ src/mocks/handlers.js
@@ -0,0 +1,21 @@
+import { rest } from 'msw';
+
+export const handlers = [
+    rest.get(
+        'http://localhost:3004/posts',
+        (req, res, ctx) => {
+            return res(
+                ctx.status(200),
+                ctx.json([
+                    {
+                        id: 1,
+                        title: 'json-server',
+                        content: 'Lorem Ipsum',
+                        author: 'typicode',
+                        image: 'https://loremflickr.com/1234/2345/cats',
+                    },
+                ]),
+            );
+        },
+    ),
+];
